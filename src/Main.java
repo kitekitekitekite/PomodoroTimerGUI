11d1042d014f8929ff3919d50dfc898008060b19
7maken <jakobkeegan1@gmail.com>
2025-10-19 14:07:47 -0700

    first

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+// creates frame
+JFrame frame = new JFrame("Pomodoro Timer");
+
+frame.setSize(300, 400);
+frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+frame.setVisible(true);
+
+    }
+}
