83f3167d7872cfb02058bbe906552811a7b3d0be
7maken <jakobkeegan1@gmail.com>
2025-10-19 15:26:29 -0700

    added actionlistener and start timer button

--- src/Main.java
+++ src/Main.java
@@ -9,10 +9,30 @@ public class Main {
 // creates frame
 JFrame frame = new JFrame("Pomodoro Timer");
 
-
+//this is a text label that goes somewhere in the interface idk where yet.
 JLabel textLabel = new JLabel("test");
 
+//make panel to hold the buttons
+JPanel timerPanel = new JPanel();
 
+//Start timer button
+        JButton startTimer = new JButton("Start Timer");
+
+startTimer.addActionListener(e -> System.out.println("Start Pomodoro Timer"));
+
+//centers the text from test jlabel
+textLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        textLabel.setVerticalAlignment(SwingConstants.VERTICAL);
+frame.add(textLabel);
+
+//add button
+        frame.add(startTimer);
+
+//add panel to frame
+frame.add(timerPanel, BorderLayout.NORTH);
+
+
+//displays the window
 frame.setSize(300, 400);
 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 frame.setVisible(true);
